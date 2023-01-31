d5af8ce50c042fcc231a4d644926e66e781bc175
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2023-01-30 18:22:28 -0600

    Add collision back

--- Scripts/Pin.gd
+++ Scripts/Pin.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 #Variables
 var velocity = Vector2(1,0)
 var speed = 100
+var hp = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +14,14 @@ func _ready():
 func _process(delta):
 	var collision = move_and_collide(speed*velocity*delta)
 	if collision:
-		# Delete if reached endzone
+		print("got collision")
 		if collision.collider.name == "EnemyBarrier":
+			# Inflict damage on player's life total
+			GlobalVars._wasHit(1000)
+			# Despawn
 			queue_free()
+		else:
+			hp-=1
+			if hp == 0:
+				# Despawn if out of HP
+				queue_free()

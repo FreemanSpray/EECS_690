b5d49755cd6e6591506978ce6be1c8b74d9eb9b6
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2023-02-02 21:05:05 -0600

    Implement pre-game instructions

--- Scenes/Instructions.gd
+++ Scenes/Instructions.gd
@@ -1,15 +1,12 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var tick = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called when unhandled input is detected
 func _unhandled_input(event):
@@ -18,6 +15,12 @@ func _unhandled_input(event):
 		get_tree().quit()
 	elif event.is_action_pressed("ui_accept"):
 		# Start game
-		get_tree().change_scene("res://Scenes/Instructions.tscn")
+		get_tree().change_scene("res://Scenes/Main.tscn")
 	# Mark input as handled so it won't trigger multiple times per keypress
 	get_tree().set_input_as_handled()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	tick+=1
+	if tick == 500:
+		get_tree().change_scene("res://Scenes/Main.tscn")

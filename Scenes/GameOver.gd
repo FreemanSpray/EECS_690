73bb426b7f1f7bfd32fea8ad8576358069930221
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2023-02-02 20:33:16 -0600

    Implement letter selector

--- Scenes/GameOver.gd
+++ Scenes/GameOver.gd
@@ -4,6 +4,9 @@ extends Control
 # var a = 2
 # var b = "text"
 var ticks = 0
+var current_selector = 0
+var letter_indices = [0,0,0]
+var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",]
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,6 +22,7 @@ func _ready():
 
 # Called when unhandled input is detected
 func _unhandled_input(event):
+	print(event)
 	if event.is_action_pressed("ui_escape"):
 		# Quit game
 		get_tree().quit()
@@ -30,6 +34,18 @@ func _unhandled_input(event):
 		GlobalVars.m_health = 1000
 		GlobalVars._score = 0
 		get_tree().change_scene("res://Scenes/Main.tscn")
+	if event.is_action_pressed("ui_up"):
+		adjust_letter("up")
+	if event.is_action_pressed("ui_down"):
+		adjust_letter("down")
+	if event.is_action_pressed("ui_left"):
+		if current_selector != 0:
+			current_selector-=1
+			$Cursor.set("rect_position", Vector2($Cursor.get("rect_position").x - 80, $Cursor.get("rect_position").y))
+	if event.is_action_pressed("ui_right"):
+		if current_selector != 2:
+			current_selector+=1
+			$Cursor.set("rect_position", Vector2($Cursor.get("rect_position").x + 80, $Cursor.get("rect_position").y))
 	# Mark input as handled so it won't trigger multiple times per keypress
 	get_tree().set_input_as_handled()
 
@@ -43,3 +59,19 @@ func _process(delta):
 		else:
 			$Cursor.show()
 		ticks = 0
+		
+func adjust_letter(direction):
+	var selector = null
+	if current_selector == 0:
+		selector = $LetterSelector1
+	elif current_selector == 1:
+		selector = $LetterSelector2
+	else:
+		selector = $LetterSelector3		
+	if direction == "up":
+		if letter_indices[current_selector] != 25:
+			letter_indices[current_selector]+=1
+	elif direction == "down":
+		if letter_indices[current_selector] != 0:
+			letter_indices[current_selector]-=1
+	selector.set("text", alphabet[letter_indices[current_selector]])
